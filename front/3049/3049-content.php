d5a2486964ce4d22debbe37d73ed508f77b9369a
batrieutran <batrieutran43@gmail.com>
2024-10-17 07:39:26 +0700

    cmt update front

---
+++ front/3049/3049-content.php
@@ -0,0 +1,106 @@
+<div class="type-3049">
+    <div class="container dev-3049">
+      <div class="main-content col-md-9">
+        <div class="article">
+          <div class="article-img"> 
+              <img src="./img/3049_1.png" alt="Smartphone repair" />
+              <div class="info">MAY 29, 2027| WORDPRESS | CAMERA | SPORT</div>
+          </div>
+          <div class="article-content">
+            <h4>10 Tips: Avoid Breaking Glass of your Smartphone</h4>
+            <p>
+              Femiration we are surrounded possession frequently them. Empilait
+              acable heureuse capitale havresac nul etroites. Tours him route
+              accabie ecarter grand. Remarkably didn't Wordpress increasing
+              occasional to difficulty especically. Known tiled he sorry joy
+              balls.
+            </p>
+            <a href="#">READ MORE <i class='fa fa-arrow-right'></i></a>
+          </div>
+        </div>
+        <div class="article">
+           <div class="article-img">
+              <img src="./img/3049_2.png" alt="Smartphone repair" />
+              <div class="info">FEBRUARY 8, 2027| WORDPRESS | CAMERA | FOOD</div>
+          </div>
+          <div class="article-content">
+            <h4>How to Repair iPhone: 6 Way Best Online Guides</h4>
+            <p>
+              Admiration we are surrounded possession frequently them. Empilait
+              acable heureuse capitable haversac nul etroites. Tours him route
+              accanle ecarter grand. Remarkably didn't Wordpress increasing
+              occasional to difficulty especically. Known tiled he sorry joy
+              balls. Bed sudden manner indeed now feebly.
+            </p>
+            <a href="#">READ MORE <i class='fa fa-arrow-right'></i></a>
+          </div>
+        </div>
+        <div class="article">
+           <div class="article-img">
+              <img src="./img/3049_3.png" alt="Smartphone repair" />
+              <div class="info">FEBRUARY 8, 2027| FOOD | SPORT</div>
+          </div>
+          <div class="article-content">
+            <h4>Finding a used Smartphone: Checking the Original</h4>
+            <p>
+              We are surrounded possession frequently them. Empilait acable
+              heureuse capitable haversac nul etroites. Tours him route acable
+              ecarter grand.
+            </p>
+            <a href="#">READ MORE <i class='fa fa-arrow-right'></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="sidebar col-md-3">
+        <div class="search-container">
+          <input type="text" placeholder="Search" />
+          <button><i class='fa fa-search'></i></button>
+        </div>
+        <div class="subscribe-box">
+          <h3>Get Even More</h3>
+          <p id="detail">" Get all latest content delivered straight to your inbox. "</p>
+          <div class="input-container">
+            <input type="email" placeholder="Enter email" />
+            <button>GO</button>
+          </div>
+          <p id="note">
+            Don't worry we don't spam
+          </p>
+        </div>
+        <div class="popular-posts">
+            <table>
+                <tr>
+                    <td class="tab-cell">POPULAR</td>
+                    <td class="tab-cell">RECENT</td>
+                </tr>
+                <tr>
+                    <td class="tab-cell">COMMENTS</td>
+                    <td class="tab-cell">TAGS</td>
+                </tr>
+            </table>
+            <div class="post">
+                <img src="./img/3049_2.png" alt="How to Repair iPhone" />
+                <div class="post-details">
+                    <a href="#">How to Repair iPhone: 6 Ways Best Online Guides</a>
+                    <p>February 9, 2017</p>
+                </div>
+            </div>
+            <div class="post">
+                <img src="./img/3049_1.png" alt="10 Tips: Avoid Breaking Glass" />
+                <div class="post-details">
+                    <a href="#">10 Tips: Avoid Breaking Glass of your Smartphone</a>
+                    <p>May 29, 2017</p>
+                </div>
+            </div>
+            <div class="post">
+                <img src="./img/3049_3.png" alt="Finding a used Smartphone" />
+                <div class="post-details">
+                    <a href="#">Finding a used Smartphone: Checking the Original</a>
+                    <p>February 9, 2017</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+</div>
---
+++ front/3065/3065-content.php
@@ -0,0 +1,76 @@
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h5 class="icon"><i class="fa fa-fax"></i>V<span>O</span>IP</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+                        quod mollitia quisquam. Architecto quam in atque sint voluptatem,
+                        consequatur consectetur ab ipsum maxime quod consequuntur
+                        excepturi illum dolorem ex modi...
+                    </p>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+                <div class="col-md-3">
+                    <h5>Company</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Categories</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Server</a></li>
+                        <li><a href="#">Uncategorized</a></li>
+                        <li><a href="#">VOIP</a></li>
+                        <li><a href="#">Voip Analytics</a></li>
+                        <li><a href="#">Voip App</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Subscribe</h5>
+                    <form>
+                        <div class="mb-3">
+                            <input type="text" placeholder="Enter your full name" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" placeholder="Enter your email address" />
+                        </div>
+                        <button type="submit" class="btn btn-info">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="footer-contact">
+            <div class="row">
+                <div class="col-md-3 col-xs-6">
+                    <span><i class="fa fa-headphones"></i></span><a href="">24/7 Customer Support</a>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <span><i class="fa fa-envelope-o"></i></span><a href="">support@example.com</a>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <span><i class="fa fa-comments-o"></i></span><a href="">Click Here To Live Chat</a>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <span><i class="fa fa-phone"></i></span><a href="">+44 000 000 000</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="footer-card">
+        <div class="container p-0 m-0">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="left">Copyright 2017 ©<a href="">BizWalls</a>.All Rights Reserved.</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="right">We Accept: <img src="http://themelooks.us/demo/bizwalls/wordpress/wp-content/uploads/2017/05/payment-methods-1.png" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </div>
